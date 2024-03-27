27951b015b6cc12318ad4e1c9d9c8173b853f966
Altti Äyräs <altti.ayras@gmail.com>
2024-03-27 18:32:20 +0200

    clean up player scene

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,8 +1,6 @@
 extends CharacterBody3D
 
-
 @export var SPEED = 5.0
-
 
 @onready var camera_controller = $CameraController
 @onready var camera_target = $CameraController/CameraTarget
@@ -29,26 +27,22 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	
 	#press esc to get mouse back
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-	#make camera follow player with lerp smoothing
+	#camera follows player with lerp smoothing
 	camera_controller.position = lerp(camera_controller.position, position, 0.15)
 	
-	#camera rotation with mouse
+	#camera rotation
 	camera_controller.rotate_y(twist_input)
 	camera_target.rotate_x(pitch_input)
 	camera_target.rotation.x = clamp(camera_target.rotation.x, deg_to_rad(-30), deg_to_rad(30))
 	twist_input = 0.0
 	pitch_input = 0.0
-	
-	
 
 
 func _unhandled_input(event):
-	
 	#moving camera with mouse
 	if event is InputEventMouseMotion:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
