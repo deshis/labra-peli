310089e8f9d2ca36871a866019ee0fd392bde6dd
Altti Äyräs <altti.ayras@gmail.com>
2024-04-19 20:16:15 +0300

    enemy movement animation

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
-@export var SPEED = 5.0
+@export var WALK_SPEED = 3.5
+@export var RUN_SPEED = 5.5
+var current_speed
 
 @onready var player = get_node('/root/Main/Player')
 @onready var nav = $NavigationAgent3D
@@ -19,8 +21,11 @@ var health = max_health
 @onready var health_bar = $HealthBar/SubViewport/TextureProgressBar
 
 var dead = false
+var close_to_player = false
 
 @onready var skeleton = $guy/DRV_Armature/Skeleton3D
+
+@onready var animation_tree = $guy/AnimationTree
 
 func _ready():
 	update_health_bar()
@@ -30,12 +35,32 @@ func _physics_process(delta):
 	var current_location
 	var new_velocity
 	
+	if(abs(global_position - player.global_position).length()<5): #if close to player
+		close_to_player = true
+		current_speed = WALK_SPEED
+	else:
+		close_to_player = false
+		current_speed = RUN_SPEED
+	
 	if aggro and not dead:
 		#calculate path towards player
 		next_location = nav.get_next_path_position()
 		current_location = global_transform.origin
-		new_velocity = (next_location - current_location).normalized() * SPEED
+		new_velocity = (next_location - current_location).normalized() * current_speed
 		nav.set_velocity(new_velocity)
+		
+		if(close_to_player or new_velocity.length()<0.001): #look at player and strafe
+			skeleton.look_at(player.global_position)
+			skeleton.rotate_object_local(Vector3.UP, PI)
+			animation_tree.set("parameters/isStrafe/blend_amount", 1.0)
+			animation_tree.set("parameters/strafe/blend_position", velocity)
+		else: #else look at navigation path direction and run
+			skeleton.look_at(to_global(new_velocity), Vector3.UP)
+			skeleton.rotate_object_local(Vector3.UP, PI)
+			animation_tree.set("parameters/isStrafe/blend_amount", 0.0)
+			animation_tree.set("parameters/idle-run/blend_position", velocity.length() / current_speed)
+		skeleton.rotation.x = 0
+		skeleton.rotation.z = 0
 	else:
 		velocity.x = 0
 		velocity.z = 0
@@ -44,15 +69,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity*delta
 	
-	if(new_velocity):
-		if(abs(global_position - player.global_position).length()<10): #if close to player, look at player
-			skeleton.look_at(player.global_position)
-			skeleton.rotate_object_local(Vector3.UP, PI)
-		else: #else look at navigation path direction
-			skeleton.look_at(to_global(new_velocity))
-			skeleton.rotate_object_local(Vector3.UP, PI)
-		skeleton.rotation.x = 0
-		skeleton.rotation.z = 0
+
 
 #calculating safe velocity for avoidance to prevent enemies clumping together
 func _on_navigation_agent_3d_velocity_computed(safe_velocity):
