ba879bd6e39d8aae3cb0ff9b7038c72c33c58ed4
deshis <altti.ayras@gmail.com>
2024-04-23 16:44:13 +0300

    changes for laptop

--- Scripts/WorldEnvironment.gd
+++ Scripts/WorldEnvironment.gd
@@ -7,7 +7,11 @@ func _ready():
 
 func update_world_environment_settings():
 	config.load("user://settings.cfg")
-	environment.ssao_enabled = config.get_value("Graphics", "ambient_occlusion")
-	environment.sdfgi_enabled = config.get_value("Graphics", "global_illumination")
-	environment.glow_enabled = config.get_value("Graphics", "glow")
-	environment.ssr_enabled = config.get_value("Graphics", "reflections")
+	if(config.get_value("Graphics", "ambient_occlusion")!=null):
+		environment.ssao_enabled = config.get_value("Graphics", "ambient_occlusion")
+	if(config.get_value("Graphics", "global_illumination")!=null):
+		environment.sdfgi_enabled = config.get_value("Graphics", "global_illumination")
+	if(config.get_value("Graphics", "glow")!=null):
+		environment.glow_enabled = config.get_value("Graphics", "glow")
+	if(config.get_value("Graphics", "reflections")!=null):
+		environment.ssr_enabled = config.get_value("Graphics", "reflections")
